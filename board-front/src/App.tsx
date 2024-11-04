f17f4f5ecbe7d3a144399b0dfcedda21d0d446d0
hyeseong-dev <hyeseong43@gmail.com>
2024-11-04 23:01:31 +0900

    feat: #15, #16 InputBox 컴포넌트 추가 및 에러 메시지 처리 기능 구현
    - InputBox 컴포넌트에 이메일 입력 필드 추가
    - props 인터페이스를 통한 타입 안전성 강화
    - 오류 상태에 따라 스타일을 조건부 렌더링하도록 구성
    - setValue 함수로 입력 값 상태 동기화
    - onKeyDown, onChange 핸들러 추가하여 사용자 입력 처리

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 import BoardItem from "components/BoardListitem"
@@ -10,9 +10,22 @@ import InputBox from 'components/InputBox';
 
 
 function App() {
+
+  // temp
+  const[value, setValue] = useState<string>('');
+
+
   return (
     <>
-      <InputBox />
+      <InputBox 
+        label='이메일' 
+        type='text' 
+        placeholder='이메일 주소를 입력해주세요' 
+        value={value} 
+        error={true} 
+        setValue={setValue} 
+        message="오류가 발생했습니다"
+      />
     </>
   );
 }
