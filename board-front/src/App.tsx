3f572b0b9a6eceb172439a1908f2872c0a589679
hyeseong-dev <hyeseong.lee@iea.co.kr>
2024-11-05 17:14:09 +0900

    feat: #17 Footer 컴포넌트 추가 및 정적 파일에 로고 이미지 추가
    - 사이트 하단에 위치하는 Footer 레이아웃 컴포넌트 생성
    - 사이트 로고, 이메일 링크, 인스타그램 및 네이버 블로그 아이콘 추가
    - 로고 및 소셜 미디어 아이콘 클릭 시 새 탭에서 열리도록 구현
    - Footer 스타일링: 로고, 링크, 저작권 정보의 폰트 및 배경 색상 설정
    - public 폴더에 로고 이미지 파일 추가 및 경로 설정

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,32 +1,12 @@
-import React, { useState } from 'react';
-
 import './App.css';
-import BoardItem from "components/BoardListitem"
-import { top3BoardListMock, latestBoardListMock, commentListMock, favoriteListMock } from 'mocks';
-import Top3Item from 'components/top3Item';
-import CommentItem from 'components/CommentItem';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/InputBox';
+import Footer from 'layouts/Footer';
 
 
 function App() {
 
-  // temp
-  const[value, setValue] = useState<string>('');
-
 
   return (
-    <>
-      <InputBox 
-        label='이메일' 
-        type='text' 
-        placeholder='이메일 주소를 입력해주세요' 
-        value={value} 
-        error={true} 
-        setValue={setValue} 
-        message="오류가 발생했습니다"
-      />
-    </>
+    <Footer />
   );
 }
 
